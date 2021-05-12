45b18de81d7e5c5a86d354da15398b8149001d3d
Magno Lomardo <magnussolidus@gmail.com>
2021-05-12 03:06:38 -0300

    feat: adicionado cobertura da classe sessão em 99,1%

--- src/main/java/com/fullteaching/backend/session/SessionTestCase.java
+++ src/main/java/com/fullteaching/backend/session/SessionTestCase.java
@@ -2,12 +2,10 @@ package com.fullteaching.backend.session;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fullteaching.backend.course.Course;
-import com.sun.jna.platform.win32.Netapi32Util.User;
 
 class SessionTestCase {
 	
@@ -15,6 +13,7 @@ class SessionTestCase {
 	private Course courseA;
 	private Session sessionA;
 	private Session sessionB;
+	private Session simpleSession;
 
 	@BeforeEach
 	void setUp() 
@@ -23,6 +22,7 @@ class SessionTestCase {
 		courseA = new Course("Programação", "prog01.png", teacher);
 		sessionA = new Session("Aula 01", "Primeira aula", Long.MIN_VALUE);
 		sessionB = new Session("Aula 02", "Segunda aula", Long.MAX_VALUE, courseA);
+		simpleSession = new Session();
 	}
 
 	@Test
@@ -47,5 +47,93 @@ class SessionTestCase {
 	void testGetCourse() {
 		assertEquals(sessionB.getCourse(), courseA);
 	}
+	
+	@Test
+	void testSimpleSession()
+	{
+		assertNotNull(simpleSession);
+	}
+	
+	@Test
+	void testGetId()
+	{
+		sessionA.setId(3);
+		assertEquals(sessionA.getId(), 3);
+	}
+	
+	@Test
+	void testSetTitle()
+	{
+		simpleSession.setTitle("Alguma coisa");
+		assertEquals(simpleSession.getTitle(), "Alguma coisa");
+	}
+	
+	@Test
+	void testSetDescription()
+	{
+		simpleSession.setDescription("descrição");
+		assertEquals(simpleSession.getDescription(), "descrição");
+	}
+	
+	@Test
+	void testSetData()
+	{
+		simpleSession.setDate(Long.MIN_VALUE);
+		assertEquals(simpleSession.getDate(), Long.MIN_VALUE);
+	}
+	
+	@Test
+	void testSetCourse()
+	{
+		simpleSession.setCourse(courseA);
+		assertEquals(simpleSession.getCourse(), courseA);
+	}
+	
+	@Test
+	void testEqualsNull()
+	{
+		assertFalse(simpleSession.equals(null));
+	}
+	
+	@Test
+	void testEqualsSelf()
+	{
+		assertTrue(simpleSession.equals(simpleSession));
+	}
+	
+	@Test
+	void testEqualsOtherSession()
+	{
+		assertTrue(simpleSession.equals(sessionA));
+	}
+	
+	@Test
+	void testEqualsSessionsId()
+	{
+		simpleSession.setId(1);
+		sessionA.setId(4);
+		assertFalse(simpleSession.equals(sessionA));
+	}
+	
+	@Test
+	void testEqualsDifferentType()
+	{
+		assertFalse(simpleSession.equals(teacher));
+	}
+	
+	@Test
+	void testToStringLengthBiggerThenZero()
+	{
+		assertNotNull(sessionA.toString());
+		assertTrue(sessionA.toString().length() > 0);
+	}
+	
+	@Test
+	void testToStringLengthLessThen512()
+	{
+		assertNotNull(sessionA.toString());
+		assertTrue(sessionA.toString().length() < 512);
+	}
+	
 
 }

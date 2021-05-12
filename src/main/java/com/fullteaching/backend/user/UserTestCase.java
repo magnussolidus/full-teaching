06a44bbaaad2058c14ee2d24e60c745903768b93
Magno Lomardo <magnussolidus@gmail.com>
2021-05-12 04:27:49 -0300

    feat: adicionado cobertura da classe User em 100%

--- src/main/java/com/fullteaching/backend/user/UserTestCase.java
+++ src/main/java/com/fullteaching/backend/user/UserTestCase.java
@@ -2,26 +2,32 @@ package com.fullteaching.backend.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.sun.jna.platform.win32.Netapi32Util.User;
+import com.fullteaching.backend.course.Course;
 
 class UserTestCase {
 	
 	private User studentA;
 	private User studentB;
 	private User teacher;
+	private User simpleUser;
+	private Course sampleCourse;
 	
 	@BeforeEach
 	void setUp()
 	{
-		studentA = new User("Magno Lomardo", "pass", null, "Student");
-		studentB = new User("Bacharelando SI", "pass", null, "Student");
-		teacher = new User("Vânia Neves", "pass", null, "Teacher");
+		studentA = new User("Magno Lomardo", "pass", "magnussolidus", null, "Student");
+		studentB = new User("Bacharelando SI", "pass", "calouro", "", "Student", "User");
+		teacher = new User("Vânia Neves", "pass", "Drª", "teacher_avatar.png", "Teacher");
+		simpleUser = new User();
+		sampleCourse = new Course("Qualidade e Teste", "q&a.png", teacher);
 	}
 
 	@Test
@@ -32,32 +38,132 @@ class UserTestCase {
 	}
 	
 	@Test
-	void testGetPasswordHash()
-	{
-		assertEquals(studentA.getPasswordHash(), new BCryptPasswordEncoder().encode("pass"));
-		assertEquals(studentB.getPasswordHash(), new BCryptPasswordEncoder().encode("pass"));
-		assertEquals(teacher.getPasswordHash(), new BCryptPasswordEncoder().encode("pass"));
-	}
-	
-	@Test
 	void testGetPicture()
 	{
 		assertEquals(studentA.getPicture(), "/../assets/images/default_session_image.png");
 		assertEquals(studentB.getPicture(), "/../assets/images/default_session_image.png");
-		assertEquals(teacher.getPicture(), "/../assets/images/default_session_image.png");
+		assertEquals(teacher.getPicture(), "teacher_avatar.png");
 	}
 	
 	@Test
-	void testeGetRoles()
+	void testeGetStudentRole()
 	{
-		ArrayList<String> studentRoles = new ArrayList<String>();
-		ArrayList<String> teacherRoles = new ArrayList<String>();
-		studentRoles.add("Student");
-		teacherRoles.add("Teacher");
-		
-		assertEquals(studentA.getRoles(), studentRoles);
-		assertEquals(studentB.getRoles(), studentRoles);
-		assertEquals(teacher.getRoles(), teacherRoles);
+		String r[] = new String[] {"Student"};
+		List<String> roles = Arrays.asList(r);
+		assertEquals(studentA.getRoles(), roles);
+	}
+	
+	@Test
+	void testGetId()
+	{
+		simpleUser.setId(Long.MIN_VALUE);
+		assertEquals(simpleUser.getId(), Long.MIN_VALUE);
+	}
+	
+	@Test
+	void testSetName()
+	{
+		simpleUser.setName("exemplo");
+		assertEquals(simpleUser.getName(), "exemplo");
+	}
+	
+	@Test
+	void testSetNickname()
+	{
+		assertEquals(studentA.getNickName(), "magnussolidus");
+	}
+	
+	@Test
+	void testToString()
+	{
+		studentA.setNickName("toString");
+		assertEquals(studentA.getNickName(), studentA.toString());
+	}
+	
+	@Test
+	void testHashcode()
+	{
+		assertEquals(studentA.hashCode(), studentA.getName().hashCode());
+	}
+	
+	@Test
+	void testSetPasswordHash()
+	{
+		simpleUser.setPasswordHash("abc123");
+		assertEquals(simpleUser.getPasswordHash(), "abc123");
+	}
+	
+	@Test
+	void testSetRoles()
+	{
+		String r[] = new String[] {"User", "Student"}; 
+		List<String> roles = Arrays.asList(r);
+		simpleUser.setRoles(roles);
+		assertEquals(simpleUser.getRoles(), roles);
+	}
+	
+	@Test
+	void testSetPicture()
+	{
+		studentB.setPicture("calouro.jpeg");
+		assertEquals(studentB.getPicture(), "calouro.jpeg");
+	}
+	
+	@Test
+	void testGetRegistrationDate()
+	{
+		assertNotNull(studentB.getRegistrationDate());
+	}
+	
+	@Test
+	void testSetRegistrationDate()
+	{
+		studentA.setRegistrationDate(Long.MIN_VALUE);
+		assertEquals(studentA.getRegistrationDate(), Long.MIN_VALUE);
+	}
+	
+	@Test
+	void testGetCourses()
+	{
+		Set<Course> aux = new HashSet<>();
+		teacher.setCourses(aux);
+		assertEquals(teacher.getCourses(), aux);
+	}
+	
+	@Test
+	void testUserEqualsNull()
+	{
+		assertFalse(studentA.equals(null));
+	}
+	
+	@Test
+	void testUserEqualsItself()
+	{
+		assertTrue(studentA.equals(studentA));
+	}
+	
+	@Test
+	void testUserEqualsDifferentObject()
+	{
+		assertFalse(studentA.equals(sampleCourse));
+	}
+	
+	@Test
+	void testUserEqualsUserCopy()
+	{
+		studentA.setId(Long.MAX_VALUE);
+		simpleUser.setId(Long.MAX_VALUE);
+		simpleUser.setName(studentA.getName());
+		assertTrue(studentA.equals(simpleUser));
+	}
+	
+	@Test
+	void testUserDiffersNameFromUserCopy()
+	{
+		studentA.setId(Long.MAX_VALUE);
+		simpleUser.setId(Long.MAX_VALUE);
+		simpleUser.setName("Manoel");
+		assertFalse(studentA.equals(simpleUser));
 	}
 
 }
